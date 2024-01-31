69dc5cec30d83ca4303e3f80975b2c86c6761f4b
BlancoFS <sergio.blanco.fernandez@cern.ch>
2024-01-31 12:05:26 +0100

    Fix typo

--- mkShapesRDF/processor/modules/btagSFProducerLatinos.py
+++ mkShapesRDF/processor/modules/btagSFProducerLatinos.py
@@ -28,7 +28,7 @@ class btagSFProducerLatinos(Module):
         self.mode = mode
 
         self.max_abs_eta = """2.49999"""
-        self.min_pt = """19.99999"""
+        self.min_pt = """20.0001"""
 
         self.inputFileName = pathToJson
         self.measurement_types = None
